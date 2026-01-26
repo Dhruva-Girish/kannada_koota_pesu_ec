0b44284a20efb264c55987475167a44826337939
Dhruva Girish <dhruvagirish9@outlook.com>
2026-01-26 22:35:19 +0530

    Add Blogs route to App component

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Blogs from "@/pages/Blogs";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import Index from "./pages/Index";
@@ -40,6 +41,7 @@ const AppContent = () => (
           <main className="flex-1">
             <Routes>
               <Route path="/" element={<Index />} />
+              <Route path="/blogs" element={<Blogs />} />
               <Route path="/events" element={<Events />} />
               <Route path="/team" element={<Team />} />
               {/* <Route path="/upis" element={<Upis />} /> */}
